f7cecea3973c27c4d203c849ce40d9238e55d63d
Shahid Afridi <115124671+Shahidafridi3303@users.noreply.github.com>
2024-11-29 20:27:33 -0600

    Implemented avatar synchronization and client movement broadcasting
    Server processes client movement updates and broadcasts position changes to all connected clients.
    Added logic to spawn avatars for new clients and synchronize them with all clients.
    Added disconnection handling to remove avatars and notify remaining clients.

--- Assets/NetworkServerProcessing.cs
+++ Assets/NetworkServerProcessing.cs
@@ -1,30 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 static public class NetworkServerProcessing
 {
+    static NetworkServer networkServer;
+
+    // Dictionary to track client positions
+    static Dictionary<int, Vector2> clientPositions = new Dictionary<int, Vector2>();
 
     #region Send and Receive Data Functions
-    static public void ReceivedMessageFromClient(string msg, int clientConnectionID, TransportPipeline pipeline)
-    {
-        Debug.Log("Network msg received =  " + msg + ", from connection id = " + clientConnectionID + ", from pipeline = " + pipeline);
 
+    public static void ReceivedMessageFromClient(string msg, int clientConnectionID, TransportPipeline pipeline)
+    {
         string[] csv = msg.Split(',');
         int signifier = int.Parse(csv[0]);
 
-        if (signifier == ClientToServerSignifiers.asd)
+        if (signifier == ClientToServerSignifiers.UpdatePosition)
         {
+            // Update client position based on received velocity
+            float velocityX = float.Parse(csv[1]);
+            float velocityY = float.Parse(csv[2]);
 
+            if (clientPositions.ContainsKey(clientConnectionID))
+            {
+                clientPositions[clientConnectionID] += new Vector2(velocityX, velocityY) * Time.deltaTime;
+
+                // Broadcast the updated position to all clients
+                string positionUpdateMsg = $"{ServerToClientSignifiers.UpdatePosition},{clientConnectionID},{clientPositions[clientConnectionID].x},{clientPositions[clientConnectionID].y}";
+                foreach (var otherClientID in networkServer.GetAllConnectedClientIDs())
+                {
+                    SendMessageToClient(positionUpdateMsg, otherClientID, TransportPipeline.ReliableAndInOrder);
+                }
+            }
         }
-        // else if (signifier == ClientToServerSignifiers.asd)
-        // {
-
-        // }
-
-        //gameLogic.DoSomething();
     }
-    static public void SendMessageToClient(string msg, int clientConnectionID, TransportPipeline pipeline)
+
+    public static void SendMessageToClient(string msg, int clientConnectionID, TransportPipeline pipeline)
     {
         networkServer.SendMessageToClient(msg, clientConnectionID, pipeline);
     }
@@ -33,47 +44,58 @@ static public class NetworkServerProcessing
 
     #region Connection Events
 
-    static public void ConnectionEvent(int clientConnectionID)
+    public static void ConnectionEvent(int clientConnectionID)
     {
-        Debug.Log("Client connection, ID == " + clientConnectionID);
+        Debug.Log($"Server: Client connected, ID: {clientConnectionID}");
+
+        // Initialize position for the new client
+        clientPositions[clientConnectionID] = new Vector2(0.5f, 0.5f);
+
+        // Notify all clients about the new client's avatar
+        foreach (var otherClientID in networkServer.GetAllConnectedClientIDs())
+        {
+            string spawnMsg = $"{ServerToClientSignifiers.SpawnAvatar},{clientConnectionID},{clientPositions[clientConnectionID].x},{clientPositions[clientConnectionID].y}";
+            SendMessageToClient(spawnMsg, otherClientID, TransportPipeline.ReliableAndInOrder);
+        }
+
+        // Send existing avatars to the new client
+        foreach (var kvp in clientPositions)
+        {
+            string spawnMsg = $"{ServerToClientSignifiers.SpawnAvatar},{kvp.Key},{kvp.Value.x},{kvp.Value.y}";
+            SendMessageToClient(spawnMsg, clientConnectionID, TransportPipeline.ReliableAndInOrder);
+        }
     }
-    static public void DisconnectionEvent(int clientConnectionID)
+
+    public static void DisconnectionEvent(int clientConnectionID)
     {
-        Debug.Log("Client disconnection, ID == " + clientConnectionID);
+        Debug.Log($"Server: Client disconnected, ID: {clientConnectionID}");
+
+        // Remove from dictionary
+        clientPositions.Remove(clientConnectionID);
+
+        // Notify all clients to remove the disconnected client's avatar
+        string removeMsg = $"{ServerToClientSignifiers.RemoveAvatar},{clientConnectionID}";
+        foreach (var otherClientID in networkServer.GetAllConnectedClientIDs())
+        {
+            SendMessageToClient(removeMsg, otherClientID, TransportPipeline.ReliableAndInOrder);
+        }
     }
 
     #endregion
 
-    #region Setup
-    static NetworkServer networkServer;
-    static GameLogic gameLogic;
-
-    static public void SetNetworkServer(NetworkServer NetworkServer)
-    {
-        networkServer = NetworkServer;
-    }
-    static public NetworkServer GetNetworkServer()
-    {
-        return networkServer;
-    }
-    static public void SetGameLogic(GameLogic GameLogic)
-    {
-        gameLogic = GameLogic;
-    }
-
-    #endregion
+    public static void SetNetworkServer(NetworkServer server) => networkServer = server;
 }
 
 #region Protocol Signifiers
 static public class ClientToServerSignifiers
 {
-    public const int asd = 1;
+    public const int UpdatePosition = 1; // Sent when a client updates their velocity
 }
 
 static public class ServerToClientSignifiers
 {
-    public const int asd = 1;
+    public const int SpawnAvatar = 1; // Sent to spawn an avatar
+    public const int UpdatePosition = 2; // Sent to update a client's position
+    public const int RemoveAvatar = 3; // Sent to remove a disconnected client's avatar
 }
-
 #endregion
-
